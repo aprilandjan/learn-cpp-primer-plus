342389c05f86cc03a0292f40a122cd9f3519d0c3
aprilandjan <maybreezes1988@gmail.com>
2018-11-08 23:48:52 +0800

    feat: prepare new lesson

---
+++ src/chapter8/4_function_override.cpp
@@ -0,0 +1,5 @@
+//
+// Created by May on 2018/11/8.
+//
+
+#include "4_function_override.h"
---
+++ src/chapter8/4_function_override.h
@@ -0,0 +1,8 @@
+//
+// Created by May on 2018/11/8.
+//
+
+#ifndef LEARN_4_FUNCTION_OVERRIDE_H
+#define LEARN_4_FUNCTION_OVERRIDE_H
+
+#endif //LEARN_4_FUNCTION_OVERRIDE_H
