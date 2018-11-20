eed267f527cde00e76f9a94904706f0472043e21
aprilandjan <maybreezes1988@gmail.com>
2018-11-20 23:43:33 +0800

    feat: add new lesson about class

---
+++ src/chapter10/1_stock_class/stock.cpp
@@ -0,0 +1,59 @@
+//
+// Created by May on 2018/11/20.
+//
+
+#include <iostream>
+
+#include "stock.h"
+
+//  持有股票的实现
+void Stock::acquire(const std::string &co, long n, double pr) {
+    company = co;
+    if (n < 0) {
+        std::cout << "Number of shares can't be negative; " << company << " shares set to 0.\n";
+        shares= 0 ;
+    } else {
+        shares = n;
+    }
+    share_val = pr;
+    //  设置 total 值
+    set_tot();
+}
+
+//  买入股票
+void Stock::buy(long num, double price) {
+    if (num < 0) {
+        std::cout << "Number of shares purchased can't be negative. Transaction is aborted.\n";
+    } else {
+        shares += num;
+        share_val = price;
+        set_tot();
+    }
+}
+
+void Stock::sell(long num, double price) {
+    using namespace std;
+    if (num < 0) {
+        cout << "Number of shares sold can't be negative. Transaction is aborted.\n";
+    } else if (num > shares) {
+        cout << "Cannot sell more than you have! Transaction is aborted.\n";
+    } else {
+        shares -= num;
+        share_val = price;
+        set_tot();
+    }
+}
+
+void Stock::update(double price) {
+    share_val = price;
+    set_tot();
+}
+
+void Stock::show() {
+    std::cout << "Company: " << company << " Shares: " << shares << "\n";
+    std::cout << " Shares Price: $" << share_val << " Total worth: $" << total_val << '\n';
+}
+
+inline void Stock::set_tot_1() {
+    std::cout << "Just another inline implementation...\n";
+}
---
+++ src/chapter10/1_stock_class/stock.h
@@ -0,0 +1,30 @@
+//
+// Created by May on 2018/11/20.
+//
+
+#ifndef LEARN_STOCK_H
+#define LEARN_STOCK_H
+
+#include <string>
+
+class Stock {
+private:
+    std::string company;
+    long shares;
+    double share_val;
+    double total_val;
+    void set_tot() {
+        //  直接将该私有函数实现放在了头文件里？...
+        //  类声明中的函数自动成为内联函数
+        total_val = shares * share_val;
+    }
+    void set_tot_1();
+public:
+    void acquire(const std::string & co, long n, double pr);
+    void buy(long num, double price);
+    void sell(long num, double price);
+    void update(double price);
+    void show();
+};
+
+#endif //LEARN_STOCK_H
