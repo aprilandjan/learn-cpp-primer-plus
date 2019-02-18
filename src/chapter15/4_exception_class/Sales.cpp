3b9aedbc72a24d590773d9f85370b1d48b0a6c55
aprilandjan <maybreezes1988@gmail.com>
2019-02-18 23:28:55 +0800

    feat: add new chapter

---
+++ src/chapter15/4_exception_class/Sales.cpp
@@ -0,0 +1,5 @@
+//
+// Created by May on 2019/2/18.
+//
+
+#include "Sales.h"
---
+++ src/chapter15/4_exception_class/Sales.h
@@ -0,0 +1,71 @@
+//
+// Created by May on 2019/2/18.
+//
+
+#ifndef LEARN_SALES_H
+#define LEARN_SALES_H
+
+#include <stdexcept>
+#include <string>
+
+namespace L15_4 {
+    class Sales {
+    public:
+        enum {
+            MONTHS = 12,
+        };
+        //  declare a type of exception, extends from logic_error
+        class BadIndex: public std::logic_error {
+        private:
+            int _bad_value;
+        public:
+            explicit BadIndex(int ix, const std::string & s = "Index Error in Sales Object\n");
+            int get_bad_value() const {
+                return _bad_value;
+            }
+            // Todo: what does "throw" mean? throwable?
+            virtual ~bad_index() throw() {};
+        };
+        explicit Sales(int y = 0);
+        Sales(int y, const double & gr, int n);
+        virtual ~Sales(){};
+        int get_year() const {
+            return _year;
+        }
+        //  数组形式取值 & 设置值
+        virtual double operator[](int i) const;
+        virtual double &operator[](int i);
+    private:
+        double _gross[MONTHS];
+        int _year;
+    };
+
+    //  sub class
+    class LabeledSales: public Sales {
+    public:
+        class NBadIndex: public Sales::BadIndex {
+        private:
+            std::string _lbl;
+        public:
+            NBadIndex(const std::string & lb, int ix, const std::string & s = "Index Error in LabeledSales Object\n");
+            const std::string & get_label_value() const {
+                return _lbl;
+            }
+            virtual ~NBadIndex() throw() {};
+        };
+        explicit LabeledSales(const std::string & lb = "none", int y = 0);
+        LabeledSales(const std::string & lb, int y, const double * gr, int n);
+        virtual ~LabeledSales() {};
+        const std::string & get_label() const {
+            return _label;
+        }
+        //  数组形式取值 & 设置值
+        virtual double operator[](int i) const;
+        virtual double &operator[](int i);
+    private:
+        std::string _label;
+    };
+}
+
+
+#endif //LEARN_SALES_H
